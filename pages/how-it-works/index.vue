24dc55cc4ba23a7954ef9756265f98bfd1b3c68c
Elayne Lemos <elayne.l.lemos@gmail.com>
2020-03-26 20:39:06 -0300

    Update pages/how-it-works/index.vue
    Co-Authored-By: Gabriel Rafael Gomes <gabrielrafael2508@hotmail.com>

--- pages/how-it-works/index.vue
+++ pages/how-it-works/index.vue
@@ -18,7 +18,8 @@
             Desssa forma, se você é um(a) participante, a partir do seu cadastro você será alertado de quando um(a) de nossos(as) mediadores(as) estiver disponível para uma conversa coletiva (com limite de pessoas) ou individual.
           </p>
           <p>
-            Já se você é um(a) mediador(a), você terá acesso ao contato de nossos participantes, poderá marcar os horários em que estará disponível para prestar auxílio e conduzir chats via WhatsApp, Hangouts ou Skype.
+            Já se você é um(a) mediador(a), poderá iniciar uma reunião online em qualquer serviço (skype, jitsi, hangouts, zoom...) de sua escolha e poderá disparar e-mails para a quantidade de participantes que desejar atender. Os participantes serão convidados automaticamente de acordo com a fila do sistema.
+            Após se cadastrar, nossa equipe de voluntários se encarregará de confirmar o seu cadastro após a análise dos dados. Então você poderá iniciar suas reuniões e contribuir com as pessoas que precisam de ajuda nesse momento difícil.
           </p>
           <p>
             Todos(as) os(as) nossos(as) mediadores(as) são psicólogos(as), psiquiatras ou psicanalistas com carteira de registro em seus respectivos conselhos. Tudo para garantir que o trabalho deles de fato proporcione conforto e bem estar para nossos(as) participantes!
